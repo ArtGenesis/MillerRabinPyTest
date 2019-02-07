63450948899b344a4f11690776eefe3bd066d353
ArtGenesis <walletdat@yahoo.com>
2019-02-07 12:55:41 +0200

    Update miller_rabin.py

--- miller_rabin.py
+++ miller_rabin.py
@@ -1,13 +1,13 @@
 from random import randint
 
 def milrab(p):
-    if p%2 == 0: return False
     if p == 2: return True
+    if p % 2 == 0: return False
     m, k = p-1, 0
     while m % 2 == 0:
-        m, k = m//2, k+1
+        m, k = m // 2, k+1
     a = randint(2, p-1)
-    x = pow(a,m,p)
+    x = pow(a, m, p)
     if x == 1 or x == p-1: return True
     while k > 1:
         x = pow(x, 2, p)
